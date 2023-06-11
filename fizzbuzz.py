da82683cb8617469f6a3d7d78a73dc60ba4f5845
colinbondi <bodhidude@icloud.com>
2023-06-11 12:42:30 -0700

    Add FizzBuzz with function to check single number

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,3 +8,14 @@ for num in range(1, 101):
         print("Buzz")
     else:
         print(num)
+
+# FizzBuzz as function to test single number
+def fizz_buzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
